df7234eed9bcea43d00cf58fcf6ffc6f4f71352e
Ronnie <rogwang123@gmail.com>
2018-06-06 12:23:27 -0700

    using a push buton to send a digitsl signal

--- demo.ino
+++ demo.ino
@@ -1,21 +1,20 @@
 //set pin as output
 
-const int LED = 9;// define LED for pin 9
+int switchPin = 8;// define LED for pin 9
+int ledPin = 13;
 
 void setup(){
   //initalize pin as output
-  pinMode(LED, OUTPUT); //set pin as output
+  pinMode(ledPin, OUTPUT); //set pin as output
+  pinMode(switchPin, INPUT); //set pin as input
  }
 
  void loop(){
-  for (int i=0; i<=255; i++)
-  {
-    analogWrite(LED, i);
-    delay(10);
-   }
-   for (int i=255; i>=0; i--)
-  {
-    analogWrite(LED, i);
-    delay(10);
-   }
+    if(digitalRead(switchPin) == HIGH){
+      digitalWrite(ledPin, HIGH);
+     }
+     else
+     {
+        digitalWrite(ledPin, LOW);
+      }  
   }
