4919238ed583421fbd27df2a3e91711e68c06ca1
Ronnie <rogwang123@gmail.com>
2018-06-06 15:25:41 -0700

    pulse train 2

--- demo.ino
+++ demo.ino
@@ -2,42 +2,29 @@ int redled = 8;
 int yellowled = 10;
 int greenled = 12;
 
+
+
 void setup() {
-  pinMode (redled, OUTPUT);
+   pinMode (redled, OUTPUT);
   pinMode (yellowled, OUTPUT);
   pinMode (greenled, OUTPUT);
-  
+
 }
 
 void loop() {
+  digitalWrite(redled, 1);
+  delay (300);
+  digitalWrite(redled, 0);
+  digitalWrite(yellowled, 1);
+  delay (300);
+  digitalWrite(yellowled, 0);
+  digitalWrite(greenled, 1);
+    delay (300);
+  digitalWrite(greenled, 0);
+    digitalWrite(yellowled, 1);
+  delay (300);
+  digitalWrite(yellowled, 0);
 
- digitalWrite (redled, 1);
- delay(9000);
-    digitalWrite (redled, 0); //stopped  
-      digitalWrite (yellowled, 1); 
-   delay (3000); // yellowled lights for 3 sec.
-   digitalWrite (yellowled, 0);
-    digitalWrite (greenled, 1);
-   delay (9000);
-   digitalWrite (greenled, 0);// greenled start flashing
-   delay (500);
-   digitalWrite (greenled, 1);
-   delay (500);    
-   digitalWrite (greenled, 0);
-   delay (500);
-   digitalWrite (greenled, 1);
-   delay (500);
-   digitalWrite (greenled, 0);
-  delay (500);
- digitalWrite (greenled, 1);
-   delay (500);
-   digitalWrite (greenled, 0);
-     delay (500);
- digitalWrite (greenled, 1);
-   delay (500);
-   digitalWrite (greenled, 0);
- 
- 
-   
-    
+
+
 }
