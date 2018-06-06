53c850a2576b3945eba9dcc0a6d4a5ebcc14a423
Ronnie <rogwang123@gmail.com>
2018-06-06 15:21:28 -0700

    traffic lights

--- demo.ino
+++ demo.ino
@@ -1,29 +1,43 @@
-/* program to create a pulse trainn of LEDs/*
- *  
- */
+int redled = 8;
+int yellowled = 10;
+int greenled = 12;
 
- int led1 = 8;
- int led2 =10;
- int led3 = 12;
-
- void setup(){
-     pinMode(led1,OUTPUT);
-     pinMode(led2,OUTPUT);
-     pinMode(led3,OUTPUT);
-  }
-
-  void loop(){
-      digitalWrite(led1,HIGH);
-      delay(500);
-      digitalWrite(led2,HIGH);
-      delay(500);
-      digitalWrite(led3,HIGH);
-      delay(500);
-      digitalWrite(led1,LOW);
-      delay(500);
-      digitalWrite(led2,LOW);
-      delay(500);
-      digitalWrite(led3,LOW);
-      delay(500);
+void setup() {
+  pinMode (redled, OUTPUT);
+  pinMode (yellowled, OUTPUT);
+  pinMode (greenled, OUTPUT);
   
-  }
+}
+
+void loop() {
+
+ digitalWrite (redled, 1);
+ delay(9000);
+    digitalWrite (redled, 0); //stopped  
+      digitalWrite (yellowled, 1); 
+   delay (3000); // yellowled lights for 3 sec.
+   digitalWrite (yellowled, 0);
+    digitalWrite (greenled, 1);
+   delay (9000);
+   digitalWrite (greenled, 0);// greenled start flashing
+   delay (500);
+   digitalWrite (greenled, 1);
+   delay (500);    
+   digitalWrite (greenled, 0);
+   delay (500);
+   digitalWrite (greenled, 1);
+   delay (500);
+   digitalWrite (greenled, 0);
+  delay (500);
+ digitalWrite (greenled, 1);
+   delay (500);
+   digitalWrite (greenled, 0);
+     delay (500);
+ digitalWrite (greenled, 1);
+   delay (500);
+   digitalWrite (greenled, 0);
+ 
+ 
+   
+    
+}
