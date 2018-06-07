b1892c0ac968ab757c0e79cb9bca4c099d416d35
Ronnie <rogwang123@gmail.com>
2018-06-07 06:41:12 -0700

    Diming a led with a potentiometer

--- demo.ino
+++ demo.ino
@@ -1,16 +1,19 @@
-//Potentiometer Reading Program
+//controlling a led brightness with a potentiometer
 const int POT=0;  //Pot on analog pin 0 
-int val = 0;      //variable to hold the analog reading from the POT
+int led = 3;     //variable to define pin(PWM)
+int val = 0;     //store vale form the pot
 
 void setup() 
 {  
-   Serial.begin(9600); 
+  pinMode(led, OUTPUT);
+  
 }
 
 void loop() 
 {
   val = analogRead(POT);  
-  Serial.println(val);  
-  delay(500); 
+  val = map(val,1,1024,1,255); // to match input with output
+  analogWrite(led, val); 
+ 
 
 }
