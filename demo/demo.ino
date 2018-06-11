98ed442d1088a9431655fb6210959aa004de3e7a
Ronnie <rogwang123@gmail.com>
2018-06-11 11:51:39 -0700

    Control servo with a potentiometer

--- demo/demo.ino
+++ demo/demo.ino
@@ -1,75 +1,27 @@
-//sketch to control servo with two buttons moving in opposite direction
+/*
+ Controlling a servo position using a potentiometer (variable resistor)
+ by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
 
-#include <Servo.h>  // add servo library
-#define sw1_pin 5   //define switch pins
-#define sw2_pin 6
+ modified on 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Knob
+*/
+
+#include <Servo.h>
+
 Servo myservo;  // create servo object to control a servo
 
-
-volatile boolean sw1 = false;
-volatile boolean sw2 = false;
-
-uint8_t sw1ButtonState = 0;
-uint8_t sw2ButtonState = 0;
-
-uint8_t lastsw1ButtonState = 0;
-uint8_t lastsw2ButtonState = 0;
-
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(sw1_pin, INPUT_PULLUP);
-  pinMode(sw2_pin, INPUT_PULLUP);
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object, sendsmthe signal
 }
 
 void loop() {
- 
-     checkIfSw1ButtonIsPressed();
-     checkIfSw2ButtonIsPressed();
-
-     if( sw1){
-      Serial.println("sw1");
-       sw1 = false;
-       myservo.write(20);
-       delay(15);
-     }
-     else if( sw2){
-      Serial.println("sw2");
-       sw2 = false;
-       myservo.write(170);
-       delay(15);
-     }
-
-                             // waits for the servo to get there
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  myservo.write(val);                  // sets the servo position according to the scaled value
+  delay(15);                           // waits for the servo to get there
 }
 
-void checkIfSw1ButtonIsPressed()
-{
-    sw1ButtonState   = digitalRead(sw1_pin);
-  
-    if (sw1ButtonState != lastsw1ButtonState)
-  {
-    if ( sw1ButtonState == 0)
-    {
-      sw1=true;
-    }
-    delay(50);
-  }
-   lastsw1ButtonState = sw1ButtonState;
- }
-
-void checkIfSw2ButtonIsPressed()
-{
-    sw2ButtonState   = digitalRead(sw2_pin);
-  
-    if (sw2ButtonState != lastsw2ButtonState)
-  {
-    if ( sw2ButtonState == 0)
-    {
-      sw2=true;
-    }
-    delay(50);
-  }
-   lastsw2ButtonState = sw2ButtonState;
- }
